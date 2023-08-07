28d46b95f2be8eab9bc9b04ff9bdc740ee83c817
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-08-07 18:53:51 -0500

    /actions

--- src/actions.js
+++ src/actions.js
@@ -10,32 +10,34 @@ export const createAction = async ({request}) => {
     }
     await fetch(`${baseUrl}/cheeses`, {
         method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(newCheeses)
-})
-return redirect("/");
+        headers: {
+            "Content-Type": "application/json"
+        },
+            body: JSON.stringify(newCheeses)
+    })
+    return redirect("/");
 }
 
 export const updateAction = async ({request, params}) => {
+    const id = params.id;
     const formData = await request.formData();
-    const newCheeses = {
+    const updatedCheeses = {
         name: formData.get("name"),
         countryOfOrigin: formData.get("countryOfOrigin"),
         image: formData.get("image"),
     }
     await fetch(`${baseUrl}/cheeses/${params.id}`, {
         method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(newCheeses)
-})
-return redirect("/");
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedCheeses)
+    })
+    return redirect("/");
 }
 
 export const deleteAction = async ({params}) => {
+    const id = params.id;
     await fetch(`${baseUrl}/cheeses/${params.id}`, {
             method: "DELETE"
         })
