33e9136201791ec606438a9f4f2fa60bb680f61c
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-08-07 18:45:32 -0500

    /create and map

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -6,23 +6,23 @@ function Index(props){
     return (
         <div>
             <h2>Create a Cheese</h2>
-            <Form action="/create-cheese" method="post">
+            <Form action="/create" method="post">
                 <input type="text" name="name" placeholder="Cheese Name" />
                 <input type="text" name="countryOfOrigin" placeholder="Country of Origin" />
                 <input type="text" name="image" placeholder="Cheese Image" />
                 <input type="submit" value="create cheese" />
             </Form>
-
-            <h2>Cheeses</h2>
-        {cheese.map(cheeses => (
-        <div key={cheeses.id} className="cheeses">
+        {cheese.map((cheeses, index) => {
+            return (
+                 <div key={cheeses.id} className="cheeses">
             <Link to={`/cheeses/${cheeses.id}`}>
                 <h1>{cheeses.name}</h1>
             </Link>
             <img src={cheeses.image} alt={cheeses.name} />
             <h3>{cheeses.countryOfOrigin}</h3>
         </div>
-    ))}
+            );
+        })}
     </div>
     )
 }
